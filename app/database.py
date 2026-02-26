1240f2f40fdb6e068ef0f57432d62857f502c1a1
olegyus <oyusopov@gmail.com>
2026-02-26 08:18:42 +0500

    Update database URL setting for async engine

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ from app.config import settings
 
 
 engine = create_async_engine(
-    settings.DATABASE_URL_POOLER,
+    settings.database_url_async,
     pool_size=settings.DB_POOL_SIZE,
     max_overflow=settings.DB_MAX_OVERFLOW,
     pool_pre_ping=True,
